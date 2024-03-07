f6f9baf5fa1c198ae69547929ed39b0596fd9fb6
birisi atuya <tushhamphrey@gamil.com>
2024-03-07 11:43:03 +0300

    Modify request headers

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ const cors = require("cors");
 const initiateStkPush = require("./routes/stkpush");
 const port = process.env.PORT;
 
-app.use(
-  cors({
-    origin: "https://vercel.com/atuyas-projects/mpesa-client",
-  })
-);
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 app.use(express.json());
 app.use("/stkpush", initiateStkPush);
 
