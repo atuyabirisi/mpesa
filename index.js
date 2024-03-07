ae14d6c93d05f24c984541c4f1a9ded741a59e64
birisi atuya <tushhamphrey@gamil.com>
2024-03-07 09:16:55 +0300

    Build testing route

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 require("dotenv").config();
-const cors = require('cors');
-const initiateStkPush = require('./routes/stkpush')
+const cors = require("cors");
+const initiateStkPush = require("./routes/stkpush");
 const port = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
-app.use('/stkpush', initiateStkPush);
+app.use("/stkpush", initiateStkPush);
+
+app.get("/", (req, res) => {
+  res.send("We Are Inside");
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
