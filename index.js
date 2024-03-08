57dc8d8146740eeb0bc380e93058593beaff2a1b
birisi atuya <tushhamphrey@gamil.com>
2024-03-08 12:44:27 +0300

    Implements cors

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const initiateStkPush = require("./routes/stkpush");
 const listenCallback = require("./routes/callback");
 const port = process.env.PORT;
 
-app.use(cors());
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
+app.use(cors());
 app.use(express.json());
 app.use("/stkpush", initiateStkPush);
 app.use("/callback", listenCallback);
