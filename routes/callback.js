e4eb17125d97300e05b58cedb9748bba6015afd3
birisi atuya <tushhamphrey@gamil.com>
2024-03-08 12:30:44 +0300

    Implemnt parsing of response

--- routes/callback.js
+++ routes/callback.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const parser = require("body-parser");
 const router = express.Router();
 
+router.use(parser.json());
 router.post("/", (req, res) => {
-  const callBackData = req.body;
+  const callBackData = req.body.Body.stkCallback;
   console.log(callBackData);
 });
 
